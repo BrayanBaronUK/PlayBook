c020ec3735ebd95758c1ad73408e4f00c657a869
Cristian Sanchez <regimensur01@gmail.com>
2019-05-18 10:32:39 -0500

    Camilo comentario : RecuperarContraseña-> recuperarContraseña

--- PlayBook/app/src/main/java/konradlorenz/edu/playbook/ventana10.java
+++ PlayBook/app/src/main/java/konradlorenz/edu/playbook/ventana10.java
@@ -11,7 +11,7 @@ public class ventana10 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.RecuperarContrasena);
+        setContentView(R.layout.recuperarContrasena);
     }
 
     public void onClick(View view) {

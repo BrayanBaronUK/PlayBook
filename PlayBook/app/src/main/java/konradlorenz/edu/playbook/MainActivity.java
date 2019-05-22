785c9ff4a333f181f74b7713b43f4d06e08d57ca
Cristian Sanchez <regimensur01@gmail.com>
2019-05-21 20:22:42 -0500

    Camilo comentario : ventana 5 -> libro_principal_recycler , se crea vista que recyla libros

--- PlayBook/app/src/main/java/konradlorenz/edu/playbook/MainActivity.java
+++ PlayBook/app/src/main/java/konradlorenz/edu/playbook/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -120,7 +119,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void onClick(View view) {
-        Intent miIntent = new Intent(MainActivity.this, ventana5.class);
+        Intent miIntent = new Intent(MainActivity.this, libros_principal_recycler.class);
         startActivity(miIntent);
     }
 
